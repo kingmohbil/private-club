6243ce506cf23cf4497fdc11854d292653a6a886
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-02-22 18:14:44 +0300

    The login controller added for express-validator and user authantication using passport

---
+++ controllers/login-controller.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const { body, validationResult } = require('express-validator');
+exports.getLogin = [
+  (req, res, next) => {
+    if (req.isAuthenticated()) return res.redirect('/');
+    next();
+  },
+  (req, res) => {
+    return res.render('login');
+  },
+];
+
+exports.postLogin = [
+  body('email', '* Must be a valid email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .escape(),
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('* Password at least must be 8 characters long')
+    .isLength({ max: 20 })
+    .withMessage('* Password at most can be 20 characters long')
+    .escape(),
+  (req, res, next) => {
+    const isValid = validationResult(req);
+    if (!isValid.isEmpty()) {
+      return res.render('login', {
+        errors: isValid.array(),
+        email: req.body.email,
+        password: req.body.password,
+      });
+    }
+    next();
+  },
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+  }),
+];
