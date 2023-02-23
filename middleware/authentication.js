bb744dba5676da221c6c2cd3754d501f786df1e6
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-02-23 18:26:14 +0300

    A notAuthanticated middleware was added for the login Controller

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,3 +4,9 @@ exports.authenticated = (req, res, next) => {
   if (req.isAuthenticated()) return next();
   return res.redirect('/login');
 };
+
+exports.notAuthenticated = (req, res, next) => {
+  debug(`Authentication status: ${req.isAuthenticated()}`);
+  if (req.isAuthenticated()) return res.redirect('/');
+  next();
+};
