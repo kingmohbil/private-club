02b06e0e59b2617bf0a3c3f8b2c2a849e81d6a5a
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-02-23 18:21:08 +0300

    A middleware created to checkAuthentication status for the user

---
+++ middleware/authentication.js
@@ -0,0 +1,6 @@
+const debug = require('debug')('authentication');
+exports.authenticated = (req, res, next) => {
+  debug(`Authentication status: ${req.isAuthenticated()}`);
+  if (req.isAuthenticated()) return next();
+  return res.redirect('/login');
+};
