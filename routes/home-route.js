73f153633e581c7c1b595c7f11b9c46161e7236b
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-02-23 23:05:02 +0300

    The vip route was added to the home router

--- routes/home-route.js
+++ routes/home-route.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const messageRouter = require('./messages-route');
+const vipRouter = require('./vip-route');
 const controller = require('../controllers/home-controller');
 const router = express.Router();
 
 router.get('/', controller.getHome);
 
 router.use('/', messageRouter);
+
+router.use('/vip', vipRouter);
 
 router.delete('/logout', controller.logOut);
 
