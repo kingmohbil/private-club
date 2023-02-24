b22a4f5902177b29001d158bf5a8e74a81eff709
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-02-24 20:28:28 +0300

    A virtual full_name field added to the user Schema

--- models/users-model.js
+++ models/users-model.js
@@ -1,27 +1,29 @@
 const monggose = require('mongoose');
 const Schema = monggose.Schema;
+const userSchema = new Schema({
+  first_name: {
+    type: String,
+    minLength: 1,
+    maxLength: 20,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    minLength: 1,
+    maxLength: 20,
+    required: true,
+  },
+  email: { type: String, required: true },
+  password: { type: String, minLength: 8, required: true },
+  vip: {
+    type: Boolean,
+    default: false,
+  },
+  messages: [Schema.Types.ObjectId],
+});
 
-module.exports = monggose.model(
-  'users',
-  new Schema({
-    first_name: {
-      type: String,
-      minLength: 1,
-      maxLength: 20,
-      required: true,
-    },
-    last_name: {
-      type: String,
-      minLength: 1,
-      maxLength: 20,
-      required: true,
-    },
-    email: { type: String, required: true },
-    password: { type: String, minLength: 8, required: true },
-    vip: {
-      type: Boolean,
-      default: false,
-    },
-    messages: [Schema.Types.ObjectId],
-  })
-);
+userSchema.virtual('full_name').get(function () {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+module.exports = monggose.model('users', userSchema);
